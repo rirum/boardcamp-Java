65045d65bda9e5b288590f775151b6b048e3144f
camille miyashita (rirum) <106037608+rirum@users.noreply.github.com>
2024-02-17 18:38:20 -0300

    Update and rename IllegalArgumentException.java to InvalidCpfException.java

--- src/main/java/com/boardcamp/api/exceptions/InvalidCpfException.java
+++ src/main/java/com/boardcamp/api/exceptions/InvalidCpfException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class IllegalArgumentException extends RuntimeException {
-    public IllegalArgumentException(String message) {
+public class InvalidCpfException extends RuntimeException {
+    public InvalidCpfException(String message) {
         super(message);
     }
 }
